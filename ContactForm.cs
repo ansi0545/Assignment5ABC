c178dc590991cc0acfb3608664a3abe43aec33cf
Ann-Sofie Haasz <ann-sofie.simonsson-haasz.0545@student.uu.se>
2024-04-19 15:37:09 +0200

    Refactor contact information handling in ContactForm and MainForm

--- ContactForm.cs
+++ ContactForm.cs
@@ -7,28 +7,13 @@ namespace Assignment5ABC
     {
         public Contact Contact { get; private set; }
 
+        #region Constructors
         public ContactForm()
         {
             InitializeComponent();
-            Contact = new Contact(); // Create a new Contact instance
+            Contact = new Contact(); 
             InitializeCountries();
-
-            // Subscribe to the FormClosing event
-            this.FormClosing += ContactForm_FormClosing;
-
-            // Subscribe to the TextChanged event for each TextBox
-            txtBoxFirstName.TextChanged += txtBoxFirstName_TextChanged;
-            txtBoxLastName.TextChanged += txtBoxLastName_TextChanged;
-            txtBoxHomePhoneContactForm.TextChanged += txtBoxHomePhoneContactForm_TextChanged;
-            txtBoxCellPhoneContactForm.TextChanged += txtBoxCellPhoneContactForm_TextChanged;
-            txtBoxEmailBusinessContactForm.TextChanged += txtBoxEmailBusinessContactForm_TextChanged;
-            txtBoxEmailPrivateContactForm.TextChanged += txtBoxEmailPrivateContactForm_TextChanged;
-            txtBoxStreet.TextChanged += txtBoxStreet_TextChanged;
-            txtBoxCity.TextChanged += txtBoxCity_TextChanged;
-            txtBoxZipCode.TextChanged += txtBoxZipCode_TextChanged;
-
-            // Subscribe to the SelectedIndexChanged event for the ComboBox
-            comboBoxCountryContactList.SelectedIndexChanged += comboBoxCountryContactList_SelectedIndexChanged;
+            SubscribeToEvents();
         }
 
         public ContactForm(Contact existingContact) : this()
@@ -42,9 +27,11 @@ namespace Assignment5ABC
 
         public ContactForm(Contact existingContact, string title) : this(existingContact)
         {
-            this.Text = title; // Set the form title
+            this.Text = title; 
         }
+        #endregion
 
+        #region Initialization Methods
         private void InitializeCountries()
         {
             foreach (var country in Enum.GetValues(typeof(Assignment5ABC.ContactFiles.Countries)))
@@ -53,6 +40,23 @@ namespace Assignment5ABC
             }
         }
 
+        private void SubscribeToEvents()
+        {
+            this.FormClosing += ContactForm_FormClosing;
+            txtBoxFirstName.TextChanged += txtBoxFirstName_TextChanged;
+            txtBoxLastName.TextChanged += txtBoxLastName_TextChanged;
+            txtBoxHomePhoneContactForm.TextChanged += txtBoxHomePhoneContactForm_TextChanged;
+            txtBoxCellPhoneContactForm.TextChanged += txtBoxCellPhoneContactForm_TextChanged;
+            txtBoxEmailBusinessContactForm.TextChanged += txtBoxEmailBusinessContactForm_TextChanged;
+            txtBoxEmailPrivateContactForm.TextChanged += txtBoxEmailPrivateContactForm_TextChanged;
+            txtBoxStreet.TextChanged += txtBoxStreet_TextChanged;
+            txtBoxCity.TextChanged += txtBoxCity_TextChanged;
+            txtBoxZipCode.TextChanged += txtBoxZipCode_TextChanged;
+            comboBoxCountryContactList.SelectedIndexChanged += comboBoxCountryContactList_SelectedIndexChanged;
+        }
+        #endregion
+
+        #region Form Field Methods
         public void ClearFields()
         {
             txtBoxLastName.Clear();
@@ -64,7 +68,7 @@ namespace Assignment5ABC
             txtBoxZipCode.Clear();
             txtBoxCity.Clear();
             txtBoxStreet.Clear();
-            comboBoxCountryContactList.SelectedIndex = -1; // Reset the combo box
+            comboBoxCountryContactList.SelectedIndex = -1;
         }
 
         internal void UpdateFormFields()
@@ -79,19 +83,18 @@ namespace Assignment5ABC
             txtBoxCity.Text = Contact.Address.City;
             txtBoxStreet.Text = Contact.Address.Street;
 
-            // Find the index of the country in the ComboBox items
             int countryIndex = comboBoxCountryContactList.Items.IndexOf(Contact.Address.Country.Replace("_", " "));
-            // Set the SelectedIndex property
             comboBoxCountryContactList.SelectedIndex = countryIndex;
         }
+        #endregion
 
+        #region Event Handlers
         private void ContactForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (this.DialogResult == DialogResult.OK)
             {
                 if (!ValidateAndShowErrors())
                 {
-                    // If the data is not valid, cancel the closing event
                     e.Cancel = true;
                 }
             }
@@ -99,29 +102,26 @@ namespace Assignment5ABC
 
         private void btnOKContactForm_Click(object sender, EventArgs e)
         {
-            // When the OK button is clicked, validate the contact data
             if (ValidateAndShowErrors())
             {
-                // If the data is valid, set the DialogResult to OK and close the form
                 this.DialogResult = DialogResult.OK;
             }
         }
 
         private void btnCancelContactForm_Click(object sender, EventArgs e)
         {
-            // When the Cancel button is clicked, confirm the cancellation
             var result = MessageBox.Show("Are you sure you want to cancel?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
             if (result == DialogResult.Yes)
             {
-                // If the user confirms, set the DialogResult to Cancel and close the form
                 this.DialogResult = DialogResult.Cancel;
                 this.Close();
             }
         }
+        #endregion
 
+        #region Validation Methods
         internal bool CheckData()
         {
-
             var emailRegex = new Regex(@"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$");
             bool isEmailValid = emailRegex.IsMatch(txtBoxEmailBusinessContactForm.Text) &&
                                 emailRegex.IsMatch(txtBoxEmailPrivateContactForm.Text);
@@ -151,7 +151,9 @@ namespace Assignment5ABC
                 return false;
             }
         }
+        #endregion
 
+        #region Text Changed Event Handlers
         private void txtBoxFirstName_TextChanged(object sender, EventArgs e)
         {
             Contact.FirstName = txtBoxFirstName.Text;
@@ -204,6 +206,7 @@ namespace Assignment5ABC
                 Contact.Address.Country = comboBoxCountryContactList.SelectedItem.ToString().Replace("_", " ");
             }
         }
+        #endregion
     }
 
     public static class Constants
--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -43,36 +43,39 @@
             // listBoxPartialData
             // 
             listBoxPartialData.FormattingEnabled = true;
-            listBoxPartialData.ItemHeight = 25;
-            listBoxPartialData.Location = new Point(2, 61);
+            listBoxPartialData.Location = new Point(1, 49);
+            listBoxPartialData.Margin = new Padding(2, 3, 2, 3);
             listBoxPartialData.Name = "listBoxPartialData";
-            listBoxPartialData.Size = new Size(801, 604);
+            listBoxPartialData.Size = new Size(642, 484);
             listBoxPartialData.TabIndex = 0;
             listBoxPartialData.SelectedIndexChanged += listBoxPartialData_SelectedIndexChanged;
             // 
             // btnAddMainForm
             // 
-            btnAddMainForm.Location = new Point(32, 677);
+            btnAddMainForm.Location = new Point(11, 554);
+            btnAddMainForm.Margin = new Padding(2, 3, 2, 3);
             btnAddMainForm.Name = "btnAddMainForm";
-            btnAddMainForm.Size = new Size(229, 34);
+            btnAddMainForm.Size = new Size(183, 27);
             btnAddMainForm.TabIndex = 2;
             btnAddMainForm.Text = "Add";
             btnAddMainForm.UseVisualStyleBackColor = true;
             // 
             // btnEditMainForm
             // 
-            btnEditMainForm.Location = new Point(291, 677);
+            btnEditMainForm.Location = new Point(223, 554);
+            btnEditMainForm.Margin = new Padding(2, 3, 2, 3);
             btnEditMainForm.Name = "btnEditMainForm";
-            btnEditMainForm.Size = new Size(223, 34);
+            btnEditMainForm.Size = new Size(178, 27);
             btnEditMainForm.TabIndex = 3;
             btnEditMainForm.Text = "Edit";
             btnEditMainForm.UseVisualStyleBackColor = true;
             // 
             // btnDeleteMainForm
             // 
-            btnDeleteMainForm.Location = new Point(550, 677);
+            btnDeleteMainForm.Location = new Point(428, 554);
+            btnDeleteMainForm.Margin = new Padding(2, 3, 2, 3);
             btnDeleteMainForm.Name = "btnDeleteMainForm";
-            btnDeleteMainForm.Size = new Size(253, 34);
+            btnDeleteMainForm.Size = new Size(202, 27);
             btnDeleteMainForm.TabIndex = 4;
             btnDeleteMainForm.Text = "Delete";
             btnDeleteMainForm.UseVisualStyleBackColor = true;
@@ -80,63 +83,69 @@
             // lblIDMainForm
             // 
             lblIDMainForm.AutoSize = true;
-            lblIDMainForm.Location = new Point(32, 17);
+            lblIDMainForm.Location = new Point(25, 13);
+            lblIDMainForm.Margin = new Padding(2, 0, 2, 0);
             lblIDMainForm.Name = "lblIDMainForm";
-            lblIDMainForm.Size = new Size(28, 25);
+            lblIDMainForm.Size = new Size(22, 20);
             lblIDMainForm.TabIndex = 5;
             lblIDMainForm.Text = "Id";
             // 
             // lblNameMainForm
             // 
             lblNameMainForm.AutoSize = true;
-            lblNameMainForm.Location = new Point(148, 17);
+            lblNameMainForm.Location = new Point(119, 13);
+            lblNameMainForm.Margin = new Padding(2, 0, 2, 0);
             lblNameMainForm.Name = "lblNameMainForm";
-            lblNameMainForm.Size = new Size(232, 25);
+            lblNameMainForm.Size = new Size(194, 20);
             lblNameMainForm.TabIndex = 6;
             lblNameMainForm.Text = "Name (Surname, first name)";
             // 
             // lblOfficePhoneMainForm
             // 
             lblOfficePhoneMainForm.AutoSize = true;
-            lblOfficePhoneMainForm.Location = new Point(435, 17);
+            lblOfficePhoneMainForm.Location = new Point(347, 13);
+            lblOfficePhoneMainForm.Margin = new Padding(2, 0, 2, 0);
             lblOfficePhoneMainForm.Name = "lblOfficePhoneMainForm";
-            lblOfficePhoneMainForm.Size = new Size(115, 25);
+            lblOfficePhoneMainForm.Size = new Size(95, 20);
             lblOfficePhoneMainForm.TabIndex = 7;
             lblOfficePhoneMainForm.Text = "Office phone";
             // 
             // lblOfficeEmailContactForm
             // 
             lblOfficeEmailContactForm.AutoSize = true;
-            lblOfficeEmailContactForm.Location = new Point(634, 17);
+            lblOfficeEmailContactForm.Location = new Point(507, 13);
+            lblOfficeEmailContactForm.Margin = new Padding(2, 0, 2, 0);
             lblOfficeEmailContactForm.Name = "lblOfficeEmailContactForm";
-            lblOfficeEmailContactForm.Size = new Size(106, 25);
+            lblOfficeEmailContactForm.Size = new Size(90, 20);
             lblOfficeEmailContactForm.TabIndex = 8;
             lblOfficeEmailContactForm.Text = "Office email";
             // 
             // lblContactDetailsMainForm
             // 
             lblContactDetailsMainForm.AutoSize = true;
-            lblContactDetailsMainForm.Location = new Point(1051, 17);
+            lblContactDetailsMainForm.Location = new Point(841, 13);
+            lblContactDetailsMainForm.Margin = new Padding(2, 0, 2, 0);
             lblContactDetailsMainForm.Name = "lblContactDetailsMainForm";
-            lblContactDetailsMainForm.Size = new Size(129, 25);
+            lblContactDetailsMainForm.Size = new Size(108, 20);
             lblContactDetailsMainForm.TabIndex = 9;
             lblContactDetailsMainForm.Text = "Contact details";
             // 
             // listViewCompleteContact
             // 
             listViewCompleteContact.AllowColumnReorder = true;
-            listViewCompleteContact.Location = new Point(809, 61);
+            listViewCompleteContact.Location = new Point(647, 49);
+            listViewCompleteContact.Margin = new Padding(2, 3, 2, 3);
             listViewCompleteContact.Name = "listViewCompleteContact";
-            listViewCompleteContact.Size = new Size(823, 604);
+            listViewCompleteContact.Size = new Size(908, 532);
             listViewCompleteContact.TabIndex = 10;
             listViewCompleteContact.UseCompatibleStateImageBehavior = false;
             listViewCompleteContact.View = View.Details;
             // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1451, 723);
+            ClientSize = new Size(1566, 680);
             Controls.Add(listViewCompleteContact);
             Controls.Add(lblContactDetailsMainForm);
             Controls.Add(lblOfficeEmailContactForm);
@@ -147,6 +156,7 @@
             Controls.Add(btnEditMainForm);
             Controls.Add(btnAddMainForm);
             Controls.Add(listBoxPartialData);
+            Margin = new Padding(2, 3, 2, 3);
             Name = "MainForm";
             Text = "Customer registry by Ann-Sofie";
             ResumeLayout(false);
